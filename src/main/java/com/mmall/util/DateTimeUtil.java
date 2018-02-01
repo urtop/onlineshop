c23d6bf54b12c64d0774b27a69dda0b363fcd10c
Mark <urtop@qq.com>
2018-02-01 18:12:56 +0800

    add product module

---
+++ src/main/java/com/mmall/util/DateTimeUtil.java
@@ -0,0 +1,31 @@
+package com.mmall.util;
+
+import org.apache.commons.lang3.StringUtils;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+import java.util.Date;
+
+public class DateTimeUtil {
+
+    public static final String STANDARD_FORMAT = "yyyy-MM-dd HH:mm:ss";
+
+    public static Date strToDate(String dateTimeStr) {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormat.forPattern(STANDARD_FORMAT);
+        DateTime dateTime = dateTimeFormatter.parseDateTime(dateTimeStr);
+        return dateTime.toDate();
+    }
+
+    public static String dateToStr(Date date) {
+        if(date == null)
+            return StringUtils.EMPTY;
+        DateTime dateTime = new DateTime(date);
+        return dateTime.toString(STANDARD_FORMAT);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(DateTimeUtil.dateToStr(new Date()));
+        System.out.println(DateTimeUtil.strToDate("2018-01-29 20:04:55"));
+    }
+}
